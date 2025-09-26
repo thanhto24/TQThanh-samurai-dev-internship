2b3050e59fc47d02322de890ff007fd48e5aa5c4
thanhto24 <thanhtovntmk@gmail.com>
2025-09-26 09:46:25 +0700

    fix: change title and icon for tag

--- frontend/src/pages/signup.tsx
+++ frontend/src/pages/signup.tsx
@@ -1,10 +1,12 @@
 import { AuthForm } from "@/components/auth-form";
+import LogoHome from "@/components/logo";
 
 export default function SignupPage() {
     return (
         <div className="bg-muted h-screen flex flex-col items-center justify-center">
             <div className="w-full max-w-sm">
-                <AuthForm type="signup" />           
+                <LogoHome />
+                <AuthForm type="signup" />
             </div>
         </div>
     )
